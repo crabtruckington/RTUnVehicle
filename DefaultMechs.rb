15749e4d46984343bd0487c98680b3db8248230a
crabtruckington <47725554+crabtruckington@users.noreply.github.com>
2021-01-19 10:50:12 -0500

    Create DefaultMechs.rb
    adding DefaultMechs.rb

---
+++ DefaultMechs.rb
@@ -0,0 +1,396 @@
+class DefaultMechs
+    DEFAULTMECHLIST =
+    {
+        1 => "{
+            \"unitType\": \"Mech\",
+            \"unitId\": \"Tagged\",
+            \"unitSimGameID\": null,
+            \"pilotId\": \"Tagged\",
+            \"pilotSimGameID\": null,
+            \"unitTagSet\": {
+              \"items\": [
+                \"unit_mech\",
+                \"{CUR_TEAM.faction}\"
+              ],
+              \"tagSetSourceFile\": \"Tags/UnitTags\"
+            },
+            \"excludedUnitTagSet\": {
+              \"items\": [
+                \"unit_superheavytank\",
+                \"unit_superheavy\",
+                \"unit_noncombatant\",
+                \"unit_legendary\",
+                \"unit_bracket_med\",
+                \"unit_bracket_high\",
+                \"unit_turret\",
+                \"unit_assault\",
+                \"unit_heavy\",
+                \"unit_medium\"
+              ],
+              \"tagSetSourceFile\": \"Tags/UnitTags\"
+            },
+            \"pilotTagSet\": {
+              \"items\": [
+                \"pilot_npc_d1\"
+              ],
+              \"tagSetSourceFile\": \"Tags/PilotTags\"
+            },
+            \"excludedPilotTagSet\": {
+              \"items\": null,
+              \"tagSetSourceFile\": \"Tags/PilotTags\"
+            },
+            \"locked\": false
+          }",
+
+        2 => "{
+            \"unitType\": \"Mech\",
+            \"unitId\": \"Tagged\",
+            \"unitSimGameID\": null,
+            \"pilotId\": \"Tagged\",
+            \"pilotSimGameID\": null,
+            \"unitTagSet\": {
+              \"items\": [
+                \"unit_mech\",
+                \"{CUR_TEAM.faction}\"
+              ],
+              \"tagSetSourceFile\": \"Tags/UnitTags\"
+            },
+            \"excludedUnitTagSet\": {
+              \"items\": [
+                \"unit_superheavytank\",
+                \"unit_superheavy\",
+                \"unit_noncombatant\",
+                \"unit_bracket_high\",
+                \"unit_advanced\",
+                \"unit_turret\",
+                \"unit_assault\",
+                \"unit_heavy\",
+                \"unit_medium\",
+                \"unit_advanced\",
+                \"unit_legendary\"
+              ],
+              \"tagSetSourceFile\": \"Tags/UnitTags\"
+            },
+            \"pilotTagSet\": {
+              \"items\": [
+                \"pilot_npc_d2\"
+              ],
+              \"tagSetSourceFile\": \"Tags/PilotTags\"
+            },
+            \"excludedPilotTagSet\": {
+              \"items\": null,
+              \"tagSetSourceFile\": \"Tags/PilotTags\"
+            },
+            \"locked\": false
+          }",
+
+        3 => "{
+            \"unitType\": \"Mech\",
+            \"unitId\": \"Tagged\",
+            \"unitSimGameID\": null,
+            \"pilotId\": \"Tagged\",
+            \"pilotSimGameID\": null,
+            \"unitTagSet\": {
+              \"items\": [
+                \"unit_mech\",
+                \"{CUR_TEAM.faction}\"
+              ],
+              \"tagSetSourceFile\": \"Tags/UnitTags\"
+            },
+            \"excludedUnitTagSet\": {
+              \"items\": [
+                \"unit_superheavytank\",
+                \"unit_superheavy\",
+                \"unit_noncombatant\",
+                \"unit_bracket_high\",
+                \"unit_turret\",
+                \"unit_assault\",
+                \"unit_heavy\",
+                \"unit_medium\",
+                \"unit_legendary\"
+              ],
+              \"tagSetSourceFile\": \"Tags/UnitTags\"
+            },
+            \"pilotTagSet\": {
+              \"items\": [
+                \"pilot_npc_d3\"
+              ],
+              \"tagSetSourceFile\": \"Tags/PilotTags\"
+            },
+            \"excludedPilotTagSet\": {
+              \"items\": null,
+              \"tagSetSourceFile\": \"Tags/PilotTags\"
+            },
+            \"locked\": false
+          }",
+
+        4 => "{
+            \"unitType\": \"Mech\",
+            \"unitId\": \"Tagged\",
+            \"unitSimGameID\": null,
+            \"pilotId\": \"Tagged\",
+            \"pilotSimGameID\": null,
+            \"unitTagSet\": {
+              \"items\": [
+                \"unit_mech\",
+                \"{CUR_TEAM.faction}\"
+              ],
+              \"tagSetSourceFile\": \"Tags/UnitTags\"
+            },
+            \"excludedUnitTagSet\": {
+              \"items\": [
+                \"unit_superheavytank\",
+                \"unit_superheavy\",
+                \"unit_noncombatant\",
+                \"unit_turret\",
+                \"unit_assault\",
+                \"unit_heavy\"
+              ],
+              \"tagSetSourceFile\": \"Tags/UnitTags\"
+            },
+            \"pilotTagSet\": {
+              \"items\": [
+                \"pilot_npc_d4\"
+              ],
+              \"tagSetSourceFile\": \"Tags/PilotTags\"
+            },
+            \"excludedPilotTagSet\": {
+              \"items\": null,
+              \"tagSetSourceFile\": \"Tags/PilotTags\"
+            },
+            \"locked\": false
+          }",
+
+        5 => "{
+            \"unitType\": \"Mech\",
+            \"unitId\": \"Tagged\",
+            \"unitSimGameID\": null,
+            \"pilotId\": \"Tagged\",
+            \"pilotSimGameID\": null,
+            \"unitTagSet\": {
+              \"items\": [
+                \"unit_mech\",
+                \"{CUR_TEAM.faction}\"
+              ],
+              \"tagSetSourceFile\": \"Tags/UnitTags\"
+            },
+            \"excludedUnitTagSet\": {
+              \"items\": [
+                \"unit_superheavytank\",
+                \"unit_superheavy\",
+                \"unit_noncombatant\",
+                \"unit_bracket_low\",
+                \"unit_turret\",
+                \"unit_assault\",
+                \"unit_heavy\"
+              ],
+              \"tagSetSourceFile\": \"Tags/UnitTags\"
+            },
+            \"pilotTagSet\": {
+              \"items\": [
+                \"pilot_npc_d5\"
+              ],
+              \"tagSetSourceFile\": \"Tags/PilotTags\"
+            },
+            \"excludedPilotTagSet\": {
+              \"items\": null,
+              \"tagSetSourceFile\": \"Tags/PilotTags\"
+            },
+            \"locked\": false
+          }",
+
+        6 => "{
+            \"unitType\": \"Mech\",
+            \"unitId\": \"Tagged\",
+            \"unitSimGameID\": null,
+            \"pilotId\": \"Tagged\",
+            \"pilotSimGameID\": null,
+            \"unitTagSet\": {
+              \"items\": [
+                \"unit_mech\",
+                \"{CUR_TEAM.faction}\"
+              ],
+              \"tagSetSourceFile\": \"Tags/UnitTags\"
+            },
+            \"excludedUnitTagSet\": {
+              \"items\": [
+                \"unit_superheavytank\",
+                \"unit_superheavy\",
+                \"unit_noncombatant\",
+                \"unit_bracket_low\",
+                \"unit_turret\",
+                \"unit_assault\"
+              ],
+              \"tagSetSourceFile\": \"Tags/UnitTags\"
+            },
+            \"pilotTagSet\": {
+              \"items\": [
+                \"pilot_npc_d6\"
+              ],
+              \"tagSetSourceFile\": \"Tags/PilotTags\"
+            },
+            \"excludedPilotTagSet\": {
+              \"items\": null,
+              \"tagSetSourceFile\": \"Tags/PilotTags\"
+            },
+            \"locked\": false
+          }",
+
+        7 => "{
+            \"unitType\": \"Mech\",
+            \"unitId\": \"Tagged\",
+            \"unitSimGameID\": null,
+            \"pilotId\": \"Tagged\",
+            \"pilotSimGameID\": null,
+            \"unitTagSet\": {
+              \"items\": [
+                \"unit_mech\",
+                \"{CUR_TEAM.faction}\",
+                \"unit_bracket_high\"
+              ],
+              \"tagSetSourceFile\": \"Tags/UnitTags\"
+            },
+            \"excludedUnitTagSet\": {
+              \"items\": [
+                \"unit_superheavytank\",
+                \"unit_superheavy\",
+                \"unit_noncombatant\",
+                \"unit_bracket_low\",
+                \"unit_turret\",
+                \"unit_assault\"
+              ],
+              \"tagSetSourceFile\": \"Tags/UnitTags\"
+            },
+            \"pilotTagSet\": {
+              \"items\": [
+                \"pilot_npc_d7\"
+              ],
+              \"tagSetSourceFile\": \"Tags/PilotTags\"
+            },
+            \"excludedPilotTagSet\": {
+              \"items\": null,
+              \"tagSetSourceFile\": \"Tags/PilotTags\"
+            },
+            \"locked\": false
+          }",
+
+        8 => "{
+            \"unitType\": \"Mech\",
+            \"unitId\": \"Tagged\",
+            \"unitSimGameID\": null,
+            \"pilotId\": \"Tagged\",
+            \"pilotSimGameID\": null,
+            \"unitTagSet\": {
+              \"items\": [
+                \"unit_mech\",
+                \"{CUR_TEAM.faction}\",
+                \"unit_bracket_high\"
+              ],
+              \"tagSetSourceFile\": \"Tags/UnitTags\"
+            },
+            \"excludedUnitTagSet\": {
+              \"items\": [
+                \"unit_noncombatant\",
+                \"unit_bracket_low\",
+                \"unit_turret\",
+                \"unit_superheavytank\",
+                \"unit_superheavy\",
+                \"unit_assault\"
+              ],
+              \"tagSetSourceFile\": \"Tags/UnitTags\"
+            },
+            \"pilotTagSet\": {
+              \"items\": [
+                \"pilot_npc_d8\"
+              ],
+              \"tagSetSourceFile\": \"Tags/PilotTags\"
+            },
+            \"excludedPilotTagSet\": {
+              \"items\": null,
+              \"tagSetSourceFile\": \"Tags/PilotTags\"
+            },
+            \"locked\": false
+          }",
+
+        9 => "{
+            \"unitType\": \"Mech\",
+            \"unitId\": \"Tagged\",
+            \"unitSimGameID\": null,
+            \"pilotId\": \"Tagged\",
+            \"pilotSimGameID\": null,
+            \"unitTagSet\": {
+              \"items\": [
+                \"unit_mech\",
+                \"{CUR_TEAM.faction}\",
+                \"unit_bracket_high\"
+              ],
+              \"tagSetSourceFile\": \"Tags/UnitTags\"
+            },
+            \"excludedUnitTagSet\": {
+              \"items\": [
+                \"unit_noncombatant\",
+                \"unit_bracket_low\",
+                \"unit_turret\",
+                \"unit_superheavytank\",
+                \"unit_superheavy\"
+              ],
+              \"tagSetSourceFile\": \"Tags/UnitTags\"
+            },
+            \"pilotTagSet\": {
+              \"items\": [
+                \"pilot_npc_d9\"
+              ],
+              \"tagSetSourceFile\": \"Tags/PilotTags\"
+            },
+            \"excludedPilotTagSet\": {
+              \"items\": null,
+              \"tagSetSourceFile\": \"Tags/PilotTags\"
+            },
+            \"locked\": false
+          }",
+
+        10 => "{
+            \"unitType\": \"Mech\",
+            \"unitId\": \"Tagged\",
+            \"unitSimGameID\": null,
+            \"pilotId\": \"Tagged\",
+            \"pilotSimGameID\": null,
+            \"unitTagSet\": {
+              \"items\": [
+                \"unit_mech\",
+                \"unit_assault\",
+                \"{CUR_TEAM.faction}\",
+                \"unit_bracket_high\"
+              ],
+              \"tagSetSourceFile\": \"Tags/UnitTags\"
+            },
+            \"excludedUnitTagSet\": {
+              \"items\": [
+                \"unit_noncombatant\",
+                \"unit_generic\",
+                \"unit_turret\",
+                \"unit_superheavytank\",
+                \"unit_superheavy\"
+              ],
+              \"tagSetSourceFile\": \"Tags/UnitTags\"
+            },
+            \"pilotTagSet\": {
+              \"items\": [
+                \"pilot_npc_d10\"
+              ],
+              \"tagSetSourceFile\": \"Tags/PilotTags\"
+            },
+            \"excludedPilotTagSet\": {
+              \"items\": null,
+              \"tagSetSourceFile\": \"Tags/PilotTags\"
+            },
+            \"locked\": false
+          }"
+    }
+
+    DEFAULTCONFIG = 1
+
+    def self.getDefaultMech(type)
+        DEFAULTMECHLIST.fetch(type, DEFAULTCONFIG)
+    end
+end
